42d2bbe999f860b3e39039cd996cfb60b5d6bc24
Miguel Salamera <salameramiguel@gmail.com>
2024-11-07 19:53:01 +0800

    feat: register the database context with sqlite option

--- src/UniverseModeTracker.API/Extensions/ServiceConfigurationsExtensions.cs
+++ src/UniverseModeTracker.API/Extensions/ServiceConfigurationsExtensions.cs
@@ -1,4 +1,5 @@
 using UniverseModeTracker.Application.Mapping;
+using UniverseModeTracker.DataAccess.Persistence.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
@@ -13,6 +14,7 @@ public static class ServiceConfigurationsExtensions
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "Universe Mode Tracker API", Version = "v1" }));
         services.AddAutoMapper(typeof(MappingProfile));
+        services.AddDbContext<DefaultDataContext>(options => options.UseSqlite(configuration.GetConnectionString("DefaultConnection")));
 
         return services;
     }
