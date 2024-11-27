8183910c9aa5618ced22ea7b88cea9575d414ec7
Miguel Salamera <salameramiguel@gmail.com>
2024-11-27 12:05:29 +0800

    feat: Implement async CRUD operations in generic repository
    - Added methods: AddAsync, UpdateAsync, DeleteAsync, GetByIdAsync, GetAllAsync to Repository class.
    - Ensured all methods are asynchronous and follow the pattern of AddAsync.

--- src/UniverseModeTracker.DataAccess/Repositories/Implementations/Repository.cs
+++ src/UniverseModeTracker.DataAccess/Repositories/Implementations/Repository.cs
@@ -1,5 +1,7 @@
 using UniverseModeTracker.DataAccess.Persistence.Context;
 using UniverseModeTracker.DataAccess.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace UniverseModeTracker.DataAccess.Repositories.Implementations;
 
@@ -20,4 +22,30 @@ public class Repository<T> : IRepository<T> where T : class
         return entity;
     }
 
+    public async Task<T> UpdateAsync(T entity)
+    {
+        _defaultDataContext.Set<T>().Update(entity);
+        await _defaultDataContext.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task<bool> DeleteAsync(int id)
+    {
+        var entity = await _defaultDataContext.Set<T>().FindAsync(id);
+        if (entity == null) return false;
+
+        _defaultDataContext.Set<T>().Remove(entity);
+        await _defaultDataContext.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task<T> GetByIdAsync(int id)
+    {
+        return await _defaultDataContext.Set<T>().FindAsync(id);
+    }
+    
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _defaultDataContext.Set<T>().ToListAsync();
+    }
 }
--- src/UniverseModeTracker.DataAccess/Repositories/Interfaces/IRepository.cs
+++ src/UniverseModeTracker.DataAccess/Repositories/Interfaces/IRepository.cs
@@ -3,4 +3,8 @@ namespace UniverseModeTracker.DataAccess.Repositories.Interfaces;
 public interface IRepository<T> where T : class
 {
     Task<T> AddAsync(T entity);
+    Task<T> UpdateAsync(T entity);
+    Task<bool> DeleteAsync(int id);
+    Task<T> GetByIdAsync(int id);
+    Task<IEnumerable<T>> GetAllAsync();
 }
