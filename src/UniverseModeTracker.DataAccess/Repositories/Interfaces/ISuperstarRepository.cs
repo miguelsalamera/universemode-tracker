97d1077281773ba13910c3bd06b8afa7aa3ce853
Miguel Salamera <salameramiguel@gmail.com>
2024-11-27 10:30:12 +0800

    feat: implement Superstar repository

---
+++ src/UniverseModeTracker.DataAccess/Repositories/Interfaces/ISuperstarRepository.cs
@@ -0,0 +1,8 @@
+using UniverseModeTracker.Domain.Entities;
+
+namespace UniverseModeTracker.DataAccess.Repositories.Interfaces;
+
+public interface ISuperstarRepository : IRepository<Superstar>
+{
+   
+}
