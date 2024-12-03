bb48df54a72e40cc26650ce0625cc51850970dd3
Miguel Salamera <salameramiguel@gmail.com>
2024-12-03 13:46:15 +0800

    refactor: update DeleteAsync to return deleted entity

--- src/UniverseModeTracker.DataAccess/Repositories/Interfaces/IRepository.cs
+++ src/UniverseModeTracker.DataAccess/Repositories/Interfaces/IRepository.cs
@@ -7,5 +7,5 @@ public interface IRepository<T> where T : class
     Task<T> GetByIdAsync(Guid id);
     Task<T> AddAsync(T entity);
     Task<T> UpdateAsync(T entity);
-    Task<bool> DeleteAsync(Guid id);
+    Task<T> DeleteAsync(Guid id);
 }
