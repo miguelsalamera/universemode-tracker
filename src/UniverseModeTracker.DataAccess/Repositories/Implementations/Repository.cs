45d47b15b32c611d893a7c880d01a66c60c8bd60
Miguel Salamera <salameramiguel@gmail.com>
2024-12-03 13:24:48 +0800

    refactor: replace int with Guid in Generic Repository
    - Updated Generic Repository to use Guid instead of int as the primary key type.
    - Refactored affected methods and entities to support the change.
    - Verified compatibility with dependent code and unit tests.

--- src/UniverseModeTracker.DataAccess/Repositories/Implementations/Repository.cs
+++ src/UniverseModeTracker.DataAccess/Repositories/Implementations/Repository.cs
@@ -14,9 +14,20 @@ public class Repository<T> : IRepository<T> where T : class
         _defaultDataContext = defaultDataContext;
     }
 
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _defaultDataContext.Set<T>().ToListAsync();
+    }
+    
+    public async Task<T> GetByIdAsync(Guid id)
+    {
+        return await _defaultDataContext.Set<T>().FindAsync(id);
+    }
+    
     public async Task<T> AddAsync(T entity)
     {
         await _defaultDataContext.Set<T>().AddAsync(entity);
+
         await _defaultDataContext.SaveChangesAsync();
 
         return entity;
@@ -25,27 +36,20 @@ public class Repository<T> : IRepository<T> where T : class
     public async Task<T> UpdateAsync(T entity)
     {
         _defaultDataContext.Set<T>().Update(entity);
+
         await _defaultDataContext.SaveChangesAsync();
+        
         return entity;
     }
 
-    public async Task<bool> DeleteAsync(int id)
+    public async Task<T> DeleteAsync(Guid id)
     {
         var entity = await _defaultDataContext.Set<T>().FindAsync(id);
-        if (entity == null) return false;
 
         _defaultDataContext.Set<T>().Remove(entity);
-        await _defaultDataContext.SaveChangesAsync();
-        return true;
-    }
 
-    public async Task<T> GetByIdAsync(int id)
-    {
-        return await _defaultDataContext.Set<T>().FindAsync(id);
-    }
-    
-    public async Task<IEnumerable<T>> GetAllAsync()
-    {
-        return await _defaultDataContext.Set<T>().ToListAsync();
+        await _defaultDataContext.SaveChangesAsync();
+
+        return entity;
     }
 }
--- src/UniverseModeTracker.DataAccess/Repositories/Interfaces/IRepository.cs
+++ src/UniverseModeTracker.DataAccess/Repositories/Interfaces/IRepository.cs
@@ -2,9 +2,10 @@ namespace UniverseModeTracker.DataAccess.Repositories.Interfaces;
 
 public interface IRepository<T> where T : class
 {
+
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T> GetByIdAsync(Guid id);
     Task<T> AddAsync(T entity);
     Task<T> UpdateAsync(T entity);
-    Task<bool> DeleteAsync(int id);
-    Task<T> GetByIdAsync(int id);
-    Task<IEnumerable<T>> GetAllAsync();
+    Task<bool> DeleteAsync(Guid id);
 }
