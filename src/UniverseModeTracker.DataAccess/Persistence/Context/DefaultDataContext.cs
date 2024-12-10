f5e214faab27709e8ffa15b0fe32a06635e22576
miguelsalamera <salameramiguel@gmail.com>
2024-12-10 10:06:55 +0800

    feat: register new entities and configurations in DefaultDataContext

--- src/UniverseModeTracker.DataAccess/Persistence/Context/DefaultDataContext.cs
+++ src/UniverseModeTracker.DataAccess/Persistence/Context/DefaultDataContext.cs
@@ -7,11 +7,21 @@ namespace UniverseModeTracker.DataAccess.Persistence.Context;
 public class DefaultDataContext : DbContext
 {
     public DefaultDataContext(DbContextOptions<DefaultDataContext> options) : base(options) {}
-
+    
+    public DbSet<Brand> Brands { get; set; }
+    public DbSet<Competitor> Competitors { get; set; }
+    public DbSet<Corner> Corners { get; set; }
+    public DbSet<Match> Matches { get; set; }
+    public DbSet<Show> Shows { get; set; }
     public DbSet<Superstar> Superstars { get; set; }
+    
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.ApplyConfiguration(new CompetitorConfiguration());
+        modelBuilder.ApplyConfiguration(new CornerConfiguration());
+        modelBuilder.ApplyConfiguration(new MatchConfiguration());
+        modelBuilder.ApplyConfiguration(new ShowConfiguration());
         modelBuilder.ApplyConfiguration(new SuperstarConfiguration());
     }
 }
