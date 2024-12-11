1b3bbb4b90359f35a34e4c7974b3aa2ce2796075
miguelsalamera <salameramiguel@gmail.com>
2024-12-11 19:19:34 +0800

    feat: register repositories in DI container

--- src/UniverseModeTracker.API/Extensions/DependencyInjectionExtensions.cs
+++ src/UniverseModeTracker.API/Extensions/DependencyInjectionExtensions.cs
@@ -11,9 +11,15 @@ public static class DependencyInjectionExtensions
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddScoped<ISuperstarService, SuperstarService>();
+        services.AddScoped<IMatchService, MatchService>();
 
         services.AddScoped(typeof(IRepository<>), typeof(Repository<>)); 
         services.AddScoped<ISuperstarRepository, SuperstarRepository>();
+        services.AddScoped<IMatchRepository, MatchRepository>();
+        services.AddScoped<ICornerRepository, CornerRepository>();
+        services.AddScoped<ICompetitorRepository, CompetitorRepository>();
+        services.AddScoped<IShowRepository, ShowRepository>();
+        services.AddScoped<IBrandRepository, BrandRepository>();
 
         return services;
     }
